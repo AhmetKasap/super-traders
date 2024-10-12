c02653a19448b6eea9a5ddeafa8be23975e0ea44
ahmetkasap <aahmetkasap@gmail.com>
2024-10-12 20:49:08 +0300

    jwt and auth middlewares added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,29 @@
 const userModel = require('../models/user.model')
 const APIError = require('../utils/Error')
 const Response = require('../utils/Response')
+const bcrypt = require('bcrypt')
+const authMiddlewares = require('../middlewares/auth.middlewares')
 
 const login = async(req,res) => {
+    const user = req.body
+    console.log(user)
 
-   
-
+    const isThereEmail = await userModel.findOne({
+        where : {email : user.email}
+    })
+    if(!isThereEmail) return new Response(null, 'check your login details').badRequest(res)
+    
+    const checkPassword = await bcrypt.compare(user.password, isThereEmail.password)
+    
+    if(checkPassword) authMiddlewares.createToken(isThereEmail,res)
+            
+    else return new Response(null, 'check your login details').badRequest(res)
 }
 
 const register = async(req,res) => {
 
     const user = req.body
+    const password = await bcrypt.hash(user.password,10)
 
     const isThereUser = await userModel.findOne({
         where : {email : user.email}
@@ -21,9 +34,10 @@ const register = async(req,res) => {
         firstName : user.firstName,
         lastName : user.lastName,
         email : user.email,
-        password : user.password
+        password : password
     })
     if(userRegistered) return new Response(null, 'registration successful').created(res)
+    else throw new APIError('an error occurred during registration', 500)
     
 }
 
---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,58 @@
+const jwt = require('jsonwebtoken')
+const Response = require('../utils/Response')
+const userModel = require('../models/user.model')
+const APIError = require('../utils/Error')
+require('dotenv').config()
+
+
+const createToken = async (user,res) => {
+    const payload = {
+        id : user.id,
+        email : user.email
+    }
+    console.log(payload)
+    const token = await jwt.sign({payload}, process.env.JWT_SECRET, {expiresIn : process.env.JWT_EXPIRES_IN, algorithm:"HS512"})
+    
+    if (token) {
+        const response = {
+            user, token
+        }
+        return new Response(response, "token created successfully, login successfully").ok(res)
+    }
+    else throw new APIError("An error occurred while creating the token", 500)
+} 
+
+const checkToken = async (req,res,next) => {
+    const bearerToken = req.headers.authorization && req.headers.authorization.startsWith('Bearer ')
+    if(! bearerToken) {
+        return new Response(null, 'Token not found, please log in.').unauthorized(res)
+    } 
+    else {
+        const token = req.headers.authorization.split(' ')[1]
+        await jwt.verify(token, process.env.JWT_SECRET, async (err,decoded) => {
+            if(err) {
+                throw new APIError("Token could not be decoded", 500)
+            }
+            else {
+                const userInfo = await userModel.findOne({where : {id : decoded.payload.id}})
+                //console.log("user Info :", userInfo)
+                if(!userInfo) {
+                    throw new APIError("User not found in the database", 404)
+                }
+                else{
+                    req.authUser = userInfo         //Either one works,
+                    //res.locals.authUser = userInfo;
+                    next()
+                }
+            }
+        })
+    }
+}
+
+
+
+
+
+module.exports = {
+    createToken,checkToken
+}
