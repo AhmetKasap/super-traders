18852ea62750dfd42ca174096d966854c53a2fa0
ahmetkasap <aahmetkasap@gmail.com>
2024-10-16 13:38:35 +0300

    trade validations created

---
+++ src/middlewares/validations/trade.validations.js
@@ -0,0 +1,32 @@
+const joi = require('joi');
+const APIError = require('../../utils/Error');
+
+const tradeValidation = async (req, res, next) => {
+  try {
+    const schema = joi.object({
+      shareId: joi.string().uuid().required()
+        .messages({
+          "string.uuid": "\"shareId\" must be a valid UUID"
+        }),
+      tradeType: joi.string().valid('BUY', 'SELL').required()
+        .messages({
+          "any.only": "\"tradeType\" must be either 'BUY' or 'SELL'"
+        }),
+      quantity: joi.number().integer().positive().required()
+        .messages({
+          "number.base": "\"quantity\" must be a number",
+          "number.positive": "\"quantity\" must be a positive number",
+          "number.integer": "\"quantity\" must be an integer"
+        })
+    });
+
+    await schema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    throw new APIError(error.details[0].message, 400);
+  }
+};
+
+module.exports = {
+  tradeValidation,
+};
--- src/routes/trade.routes.js
+++ src/routes/trade.routes.js
@@ -3,9 +3,10 @@ const router = express.Router()
 
 const {buy,sell} = require('../controllers/trade.controller')
 const userAuthMiddelwares = require('../middlewares/auth/user.auth.middlewares') 
+const {tradeValidation} = require('../middlewares/validations/trade.validations')
 
-router.post('/buy', userAuthMiddelwares.checkToken, buy)
-router.post('/sell', userAuthMiddelwares.checkToken, sell)
+router.post('/buy', userAuthMiddelwares.checkToken, tradeValidation, buy)
+router.post('/sell', userAuthMiddelwares.checkToken, tradeValidation, sell)
 
 
 
