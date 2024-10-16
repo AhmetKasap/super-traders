7fb0e40233b7cb46824ec6fcd30ad56987c7194d
ahmetkasap <aahmetkasap@gmail.com>
2024-10-16 13:22:16 +0300

    user validations added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const {login,register} = require('../controllers/user.controller')
+const {registerValidation,loginValidation} = require('../middlewares/validations/user.auth.validations')
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', registerValidation, register)
+router.post('/login', loginValidation, login)
 
 
 
